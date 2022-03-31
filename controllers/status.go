743dfec76a3c7706d8d3c3d56303690f4e98c4f3
FlyingDogFood <daniel.brunsch99@gmx.de>
2022-03-31 19:57:13 +0000

    add status type

---
+++ controllers/status.go
@@ -0,0 +1,23 @@
+package controllers
+
+import (
+	"encoding/json"
+
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+type Status map[string]interface{}
+
+func newStatus() Status {
+	status := make(Status)
+	status["reconciledAt"] = metav1.Now()
+	return status
+}
+
+func (s *Status) bytes() ([]byte, error) {
+	return json.Marshal(s)
+}
+
+func (s *Status) merge(status Status, name string) {
+	(*s)[name] = status
+}
