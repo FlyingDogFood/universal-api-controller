fd1d83fc060865d8101a8b0f03d4afaa0db82db4
FlyingDogFood <daniel.brunsch99@gmx.de>
2022-04-10 19:10:18 +0000

    add templateString function

---
+++ util/templating.go
@@ -0,0 +1,19 @@
+package util
+
+import (
+	"bytes"
+	"html/template"
+)
+
+func templateString(templateString string, parameters Parameters) (string, error) {
+	templatevar, err := template.New("template").Parse(templateString)
+	if err != nil {
+		return "", err
+	}
+	var byteBuffer bytes.Buffer
+	err = templatevar.Execute(&byteBuffer, parameters)
+	if err != nil {
+		return "", err
+	}
+	return byteBuffer.String(), nil
+}
